54e9803149e9028c84c6a1f93d4b02c4a735a107
Amoora <ammartech238@gmail.com>
2023-09-21 22:08:38 -0700

    A

--- shell.c
+++ shell.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
  * run_command - forks a new process to run the command
  *
  * @args: array of arguments from the stdin
@@ -12,74 +11,14 @@
  */
 
 int run_command(char *args[], char *env[], char *path)
-=======
-* main_shell - main shell function
-*
-* @x1: name of the executable file
-*
-* Return: 0 (Exit_Success), negative (Exit_Failure)
-*/
-
-int main_shell(char *x1)
->>>>>>> 83ea31a0f9f100e5bb64a56bf5d49d0bcc5c0454
 {
 	pid_t cpid = -1;
 	int Wstatus, retStats = 0, f_accss;
 	char *fpath = full_path(args[0], path);
 
-<<<<<<< HEAD
 	retStats = check_command(fpath, args);
 	if (!fpath)
 		return (ERR_NOT_FOUND);
-=======
-	signal(SIGINT, sigint_handler);
-	if (e1)
-		_puts(PROMPT);
-
-	while ((c1 = getline(&y1, &d1, stdin)) != -1)
-	{
-		if (y1)
-		{
-			a1++;
-			z1 = trim_string(y1);
-
-			if (z1 && _strcmp(z1, "exit") == 0)
-				free(z1), free(y1), _exit(EXIT_STATUS);
-			if (z1)
-				b1 = shell_handler(z1);
-
-			if (b1 == ERROR_NOT_FOUND)
-				not_found_err(x1, z1, a1);
-			else if (b1 == PERM_DENIED)
-				perm_denied_err(x1, z1, a1);
-			else
-				EXIT_STATUS = b1;
-
-			if (z1)
-				free(z1);
-		}
-			if (e1)
-				_puts(PROMPT);
-	}
-	free(y1);
-	if (e1)
-		_putchar('\n');
-	exit(EXIT_STATUS);
-}
-
-/*
-* @f: array of arguments from the stdin
-* @g: environment of the current running process
-* @h: environment path
-*
-* Return: 0 (Success), negative number (Failure)
-*/
-int execute_command(char *f[], char *g[], char *h)
-{
-	pid_t i = -1;
-	int j, k = 0, l;
-	char *m = full_path(f[0], h);
->>>>>>> 83ea31a0f9f100e5bb64a56bf5d49d0bcc5c0454
 
 	f_accss = access(fpath, F_OK);
 	if (f_accss != -1)
@@ -164,14 +103,9 @@ int shell(char *prog_name)
 	size_t n = 0;
 	const int IS_ATTY = isatty(STDIN_FILENO);
 
-<<<<<<< HEAD
 	signal(SIGINT, sigint_handler);
 	if (IS_ATTY)
-		_puts(PROMPT);
-=======
-	r = execute_command(o, __environ, q);
-	free(q);
->>>>>>> 83ea31a0f9f100e5bb64a56bf5d49d0bcc5c0454
+	_puts(PROMPT);
 
 	while ((gtret = getline(&command, &n, stdin)) != -1)
 	{
@@ -199,7 +133,7 @@ int shell(char *prog_name)
 	}
 	free(command);
 	if (IS_ATTY)
-		_putchar('\n');
+	_putchar('\n');
 	exit(EXIT_ST);
 }
 
